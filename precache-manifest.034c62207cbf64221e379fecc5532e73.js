f33cc54b4d98fc219b2ef5f87def5d00dc73cf4c
Alexander Doroshenko <ale.doroshenko@gmail.com>
2020-06-11 12:02:47 +0200

    Add static

---
+++ precache-manifest.034c62207cbf64221e379fecc5532e73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde4a06a879920748b9cdbe1187bc9e2",
+    "url": "/memory/index.html"
+  },
+  {
+    "revision": "66863069ebc0fc55b304",
+    "url": "/memory/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "52c1bc84f149fbb32c06",
+    "url": "/memory/static/js/2.acf293c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/memory/static/js/2.acf293c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66863069ebc0fc55b304",
+    "url": "/memory/static/js/main.ee32f0f2.chunk.js"
+  },
+  {
+    "revision": "86a20cc1c0d8e24c2eab",
+    "url": "/memory/static/js/runtime-main.555dc6da.js"
+  }
+]);
